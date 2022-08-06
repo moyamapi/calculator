a7a81949a040eb34ce6d316eb12a414b1bf2af8c
moyamapi <otsuka.maki0821@gmail.com>
2022-08-06 14:11:16 +0900

    電卓

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const result = document.getElementById("result")
 
 function edit(value) {
-    if (result.value.slice(1)==="0"){
-        result.value = result.value + value;
-    }
-    else{result.value = result.value + value;}
+　　　result.value = result.value + value;
   }
 function calc() {
     result.value = new Function(
@@ -14,3 +11,14 @@ function calc() {
 function reset() {
     result.value = "";
   }  
+function operator(value){
+    if (result.value.slice(-1)==='+'){
+        return;
+    } else if (result.value.slice(-1)==='-'){
+        return;
+    } else if (result.value.slice(-1)==='*'){
+        return;
+    } else if (result.value.slice(-1)==='/'){
+        return;
+    } else {result.value = result.value + value;}
+}
