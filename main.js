62b5a327563e3614de35c18b2369f6144ee6762a
moyamapi <otsuka.maki0821@gmail.com>
2022-08-08 14:58:22 +0900

    0の制御ずみ

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const result = document.getElementById("result")
 
 function edit(value) {
-    if (result.value==='0.'||result.value==='0+'||result.value==='0-'||result.value==='0*'||result.value==='0/'){
-        result.value = result.value + value;
-    }else if(result.value==='0'){
+if(result.value==='0' && value!=='.'){
         result.value=value;
     }else {result.value = result.value + value;}
   }
